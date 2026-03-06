ef33d0b2f7c6ab20a106ff662543ae9778c3fec4
AuraMind <william.connects@outlook.com>
2026-03-06 10:58:03 -0500

    Update sync script

--- .github/workflows/assets/env.sh
+++ .github/workflows/assets/env.sh
@@ -1,9 +1,7 @@
 # Common variables for all workflows. Source this file before lib.sh.
-# In GitHub Actions, GITHUB_REPOSITORY is set; we derive ORG and TRANSLATIONS_REPO.
-# For local trigger scripts, set GITHUB_REPOSITORY (e.g. in .env) to the repo to trigger (owner/name).
 
-ORG="${ORG:-${GITHUB_REPOSITORY%%/*}}"
-TRANSLATIONS_REPO="${TRANSLATIONS_REPO:-${GITHUB_REPOSITORY##*/}}"
+ORG="${GITHUB_REPOSITORY%%/*}"
+TRANSLATIONS_REPO="${GITHUB_REPOSITORY##*/}"
 
 BOT_NAME="Boost-Translation-CI-Bot"
 BOT_EMAIL="Boost-Translation-CI-Bot@$ORG.local"
